65666e842736dd3c4c8656fb21bcf3c398503280
SobrancelhaDoDragao <eudson.duraes@gmail.com>
2023-03-11 10:29:45 -0300

    arrumando arquivos estaticos

--- PredictModel/PredictModel/settings.py
+++ PredictModel/PredictModel/settings.py
@@ -139,9 +139,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS =[BASE_DIR,"model/static"]
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- PredictModel/staticfiles/model/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('PredictDigit', views.PredictDigit, name='PredictDigit'),
-    path('get_csrf',views.get_csrf, name="get_csrf")
-]
