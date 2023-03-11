62017be3d9fd3fa9c79d8ad5bf0981e98c6c0ce7
SobrancelhaDoDragao <eudson.duraes@gmail.com>
2023-03-11 15:02:03 -0300

    Adicionando Whitenoise

--- PredictModel/settings.py
+++ PredictModel/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'corsheaders.middleware.CorsMiddleware',
@@ -141,11 +142,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'model/static'),
+  os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "model/staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- PredictModel/wsgi.py
+++ PredictModel/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PredictModel.settings')
 
 application = get_wsgi_application()
+
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
