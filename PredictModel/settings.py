aae806f79ee3224e3f2bfbfdea0726848b473d99
SobrancelhaDoDragao <eudson.duraes@gmail.com>
2023-03-11 12:55:39 -0300

    .

--- PredictModel/settings.py
+++ PredictModel/settings.py
@@ -142,12 +142,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "model/static")
-]
-
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
